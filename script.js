0adf2b8779235d07a0f4519d9f21b753d4b58232
JoshtKx <[email protected]>
2024-08-19 18:01:11 +0800

    add js for calculations

--- script.js
+++ script.js
@@ -26,15 +26,92 @@ let opr_dic = {
 };
 
 function operate(fnum,opr,snum){
-    return opr_dic[opr](fnum,snum);
+    return "" + opr_dic[opr](fnum,snum);
 }
 let display_eqn = document.querySelector(".eqn");
+let prev_eqn = document.querySelector(".prev-eqn");
+let prev_val = "";
 let display_val = "";
 let numbers = document.querySelectorAll(".num");
+
+let opr_select = false;
+let full_eqn = false;
+let dividing = false;
+let decimal = false;
+
+function clear(){
+    opr_select = false;
+    full_eqn = false;
+    dividing = false;
+    decimal = false;
+    display_val = "";
+    display_eqn.textContent = "0";
+    prev_eqn.textContent = "";
+    prev_val = ""
+}
+
+const clear_button = document.querySelector(".clear");
+clear_button.addEventListener("click",() =>{clear()});
+
+const delete_button = document.querySelector(".del");
+delete_button.addEventListener("click",()=>{
+    display_val = display_val.slice(0,-1);
+    display_eqn.textContent = display_val;
+});
+
 numbers.forEach((num) =>{
     num.addEventListener("click",() =>{
+        if (dividing && num.textContent == "0"){
+            alert("Cannot divide by 0!");
+            clear();
+        }
+        if (opr_select){
+            full_eqn = true;
+        }
+
         display_val+= num.textContent;
         display_eqn.textContent = display_val;
     });
 });
 
+let operators = document.querySelectorAll(".opr");
+operators.forEach((op) =>{
+    op.addEventListener("click",()=>{
+        if (op.textContent == "×") {
+            opr = "*";
+        }
+        else if (op.textContent == "÷"){
+            opr = "/";
+            dividing = true;
+        }
+        else{
+            opr = op.textContent;
+        }
+        if (display_val.slice(-1) == "."){
+            display_val = display_val.slice(0,-1);
+            display_eqn.textContent = display_val;
+        }
+        opr_select = true;
+        prev_val = display_val;
+        prev_eqn.textContent = (display_eqn.textContent|| "0" )+ " " + op.textContent;
+        display_val = "";
+        console.log(opr);
+    });
+});
+
+let eval = document.querySelector(".equal");
+eval.addEventListener("click", ()=>{
+    if (full_eqn){
+            prev_eqn.textContent += " " + display_eqn.textContent + " =";
+            display_val =  Math.round(operate(Number(prev_val),opr,Number(display_val))*100000)/100000;
+            display_eqn.textContent = display_val;
+        }
+    });
+let dot = document.querySelector(".decimal");
+dot.addEventListener("click",()=>{
+    if (!decimal){
+        decimal = true;
+        display_val += ".";
+        display_eqn.textContent = display_val;
+    }
+});
